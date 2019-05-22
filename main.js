41f6f887ba4936d426d9ee84cf4cbb5dbeff2c43
Mitch Malinin <mitchmalinin@gmail.com>
2019-05-22 02:27:21 -0400

    Finished Score

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 // Making Rock Paper Sicciors
-
+var compCount = 0;
+var playerCount = 0;
 var playerSelection = 'rock';
 var computerSelection = 'test';
 var rock = document.getElementById('btn1');
@@ -9,6 +10,9 @@ var scissors = document.getElementById('btn3');
 var playerLabel = document.getElementById('player');
 var compLabel = document.getElementById('computer');
 var winnerLabel = document.getElementById('winner');
+var playerScore = document.getElementById('playerScore');
+var compScore = document.getElementById('computerScore');
+var finalWinner = document.getElementById('finalWinner');
 
 rock.addEventListener('click', function(e){
     
@@ -17,6 +21,7 @@ rock.addEventListener('click', function(e){
 
     displayLabels()
     results(); 
+   
     
  });
 
@@ -55,16 +60,49 @@ function displayLabels(){
 
 //results
 function results(){
+
     if(playerSelection == computerSelection){
         winnerLabel.innerHTML = ("You tied, try again.");
+
     }
 
     else if((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
     (playerSelection == 'Paper' && computerSelection == 'Rock') ||
     (playerSelection == 'Scissors' && computerSelection == 'Paper')){
-        winnerLabel.innerHTML = ("Congratulations, you win!")
+        winnerLabel.innerHTML = ("Congratulations, you WIN!")
+        playerCount++;
+        playerScore.innerHTML = ("Player Score: " + playerCount);
+        score();
+        
     }
     else{
         winnerLabel.innerHTML = ("You loose, try again.");
+        compCount++;
+        compScore.innerHTML = ("Computer Score: " + compCount);
+        score();
+        
     }
 }
+
+//Score Bored
+function score(){
+
+        if(playerCount > compCount && playerCount + compCount == 5){
+            reset();
+            finalWinner.innerHTML = ("Player Wins");
+        }
+        else if(playerCount < compCount && playerCount + compCount == 5){
+            reset();
+            finalWinner.innerHTML = ("Computer Wins");
+        }
+            
+}
+function reset(){
+    
+        playerCount = 0;
+        compCount = 0;
+        playerScore.innerHTML = ("Player Score: " + playerCount);
+        compScore.innerHTML = ("Computer Score: " + playerCount);
+        finalWinner.innerHTML = ("");
+
+        }
